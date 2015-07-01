7ea27b5d255e63b3315fc4625b1b09c25d3b8a03
flan <flan@flande.re>
2015-07-01 12:06:55 +0200

    add missing notice

--- ugoira-play/frame.h
+++ ugoira-play/frame.h
@@ -1,3 +1,9 @@
+/*
+ * This work is subject to the CC0 1.0 Universal (CC0 1.0) Public Domain
+ * Dedication license. Its contents can be found in the LICENSE file or at
+ * <http://creativecommons.org/publicdomain/zero/1.0/>
+ */
+
 #include <SDL.h>
 
 typedef struct frame_t {
