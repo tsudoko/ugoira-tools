a1fd8a1795a27ebd75daa16579e69b4eafd23780
flan <flan@flande.re>
2015-07-08 19:32:17 +0200

    add a missing header

---
+++ ugoira-play/json.h
@@ -0,0 +1,7 @@
+/*
+ * This work is subject to the CC0 1.0 Universal (CC0 1.0) Public Domain
+ * Dedication license. Its contents can be found in the LICENSE file or at
+ * <http://creativecommons.org/publicdomain/zero/1.0/>
+ */
+
+void get_frame_durations(Node *node, const char *filename);
