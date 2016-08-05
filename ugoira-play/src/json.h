2dc93d64a57375d46e9f47852846278b39010070
flan <flan@flande.re>
2016-08-05 16:11:21 +0200

    remove unused file

--- ugoira-play/src/json.h
+++
@@ -1,7 +0,0 @@
-/*
- * This work is subject to the CC0 1.0 Universal (CC0 1.0) Public Domain
- * Dedication license. Its contents can be found in the LICENSE file or at
- * <http://creativecommons.org/publicdomain/zero/1.0/>
- */
-
-void get_frame_durations(Node *node, const char *filename);
