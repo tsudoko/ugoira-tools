cc07b62242699a7f28af27ea02d05d12911e88b0
flan <flan@flande.re>
2017-05-20 04:11:20 +0200

    use the api to download files

--- ugoira-dl/ugoira-dl.py
+++ ugoira-dl/ugoira-dl.py
@@ -6,71 +6,52 @@
 
 from getpass import getpass
 from getopt import getopt
-import http.cookiejar
 import json
 import os.path
 import re
-import shutil
 import sys
 import zipfile
 
 import requests
 
-SMALL_REGEX = "pixiv\.context\.ugokuIllustData            = (.*);pixiv"
-SMALL_REGEX_NOLOGIN = "pixiv\.context\.ugokuIllustData  = (.*\}\]\});"
-LARGE_REGEX = "pixiv\.context\.ugokuIllustFullscreenData  = (.*\}\]\});"
-
-
-def save_cookies(cookies, filename):
-    cookiejar = http.cookiejar.MozillaCookieJar(filename)
-    for c in cookies:
-        args = dict(vars(c).items())
-        args['rest'] = args['_rest']
-        del args['_rest']
-
-        c = http.cookiejar.Cookie(**args)
-        cookiejar.set_cookie(c)
-    cookiejar.save(filename)
+CLIENT_ID = "bYGKuGVw91e0NMfPGp44euvGt59s"
+CLIENT_SECRET = "HP3RmkgAmEGro0gn1x9ioawQE8WMfvLXDz3ZqxpK"
 
 
 def pixiv_login(username, password):
-    s = requests.Session()
-
     data = {
-        "mode": "login",
-        "pixiv_id": username,
-        "pass": password,
-        "skip": 1,
+        "username": username,
+        "password": password,
+        "grant_type": "password",
+        "client_id": CLIENT_ID,
+        "client_secret": CLIENT_SECRET,
     }
 
-    s.post("https://www.secure.pixiv.net/login.php", data=data)
+    r = requests.post("https://oauth.secure.pixiv.net/auth/token", data=data)
+    r = r.json()
 
-    return s.cookies
+    if "response" not in r or "access_token" not in r['response']:
+        raise Exception("unexpected json:\n" + str(r))
+
+    return r['response']['access_token']
 
 
-# TODO: R-18 support, requires API login
-def get_orig_frame_url(id_):
-    params = {"illust_id": id_}
-    detail = requests.get("https://app-api.pixiv.net/v1/illust/detail",
-                          params=params).json()
-
-    if not detail.get("illust") \
-    or not detail['illust'].get("meta_single_page") \
-    or not detail['illust']['meta_single_page'].get("original_image_url"):
-        print("%s: get_frames_api: unexpected json: %s" %
-              (os.path.basename(sys.argv[0]), detail), file=sys.syderr)
+def get_orig_frame_url(metadata):
+    if not metadata.get("image_urls") or not metadata['image_urls'].get("large"):
+        print("unexpected json (no ['image_urls']['large']):\n", metadata)
         return None
 
-    url = detail['illust']['meta_single_page']['original_image_url']
+    url = metadata['image_urls']['large']
+
     return url.replace("_ugoira0.", "_ugoira{}.")
 
 
-def dl_orig(id_, metadata):
-    headers = {"Referer": "https://www.pixiv.net/"}
+def dl_orig(metadata):
+    headers = {"Referer": "https://public-api.secure.pixiv.net/"}
     filename_format = "%06d.%s"
-    zipname = "{}_ugoiraorig.zip".format(id_)
+    zipname = "{}_ugoiraorig.zip".format(metadata['id'])
 
-    url = get_orig_frame_url(id_)
+    url = get_orig_frame_url(metadata)
 
     if url is None:
         return None
@@ -78,8 +59,9 @@ def dl_orig(id_, metadata):
     ext = url.split('.')[-1]
     frames = []
 
-    for i in range(len(metadata['frames'])):
+    for i in range(len(metadata['metadata']['frames'])):
         print("GET " + url.format(i))
+        metadata['metadata']['frames'][i]['file'] = filename_format % (i, ext)
 
         r = requests.get(url.format(i), headers=headers)
         if r.status_code != 200:
@@ -96,18 +78,23 @@ def dl_orig(id_, metadata):
     return zipname
 
 
-def dl_zip(src, regex, cookies=None):
-    headers = {"Referer": "https://www.pixiv.net/"}
+def dl_zip(metadata, size="600x600"):
+    if not metadata['metadata'].get("zip_urls") or len(metadata['metadata']['zip_urls']) < 0:
+        print("unexpected json (no ['metdata']['zip_urls']):\n", metadata)
+        return None
 
-    filename = os.path.basename(src)
+    url = metadata['metadata']['zip_urls'][0]
+    url = re.sub("ugoira[0-9]+x[0-9]", "ugoira" + size, url)
 
-    if regex is not LARGE_REGEX:
-        print("%s: downloading small version of %s" %
-              (os.path.basename(sys.argv[0]), filename))
+    headers = {"Referer": url}
+    filename = os.path.basename(url)
 
-    print("GET", src)
+    print("GET", url)
+    r = requests.get(url, headers=headers, stream=True)
 
-    r = requests.get(src, headers=headers, stream=True)
+    if r.response_code != 200:
+        print("unexpected response:", r.text)
+        return None
 
     with open(filename, "wb") as f:
         for chunk in r.iter_content(chunk_size=4096):
@@ -118,91 +105,85 @@ def dl_zip(src, regex, cookies=None):
     return filename
 
 
-def get_metadata(text, regex=SMALL_REGEX_NOLOGIN):
-    metadata = re.search(regex, text)
+def dl(id_, token):
+    headers = {"Authorization": "Bearer " + token}
+    params = {"image_sizes": "large"}
+    url = "https://public-api.secure.pixiv.net/v1/works/{}.json".format(id_)
+    r = requests.get(url, headers=headers, params=params).json()
 
-    if metadata is None:
-        raise Exception("couldn't find metadata")
+    if "response" not in r or len(r['response']) < 1:
+        print("unexpected json:\n" + str(r), file=sys.stderr)
+        return False
 
-    return metadata.group(1)
+    data = r['response'][0]
+    metadata = {"frames": []}
 
+    for i in data['metadata']['frames']:
+        metadata['frames'].append({"delay": i['delay_msec']})
 
-def dl(url, cookies=None):
-    if cookies:
-        regex = LARGE_REGEX
-    else:
-        regex = SMALL_REGEX_NOLOGIN
+    filename = dl_orig(data)
 
-    r = requests.get(url, cookies=cookies)
-
-    metadata = get_metadata(r.text, regex)
-
-    src = json.loads(metadata)['src']
-    ugoname = src.split('/')[-1]
-
-    filename = dl_orig(re.sub("_.*", "", ugoname), json.loads(metadata)) or \
-               dl_zip(src, regex, cookies)
+    if filename:
+        for j, i in enumerate(data['metadata']['frames']):
+            metadata['frames'][j]['file'] = i['file']
+    else:  # TODO: zips from pixiv (600x600, 1920x1080)
+        return False
 
     metadata_filename = filename[:-4] + ".json"
 
     with open(metadata_filename, "w") as f:
-        f.write(metadata)
+        json.dump(metadata, f)
 
-    print(filename, "done")
-    return filename, metadata_filename
+    return True
 
 
 def main():
-    OPTS_SHORT = "c:p:s:u:Uv"
-    OPTS_LONG = ["cookie-jar=", "password=", "username=", "session-id=",
-                 "unattended", "keep-original"]
+    OPTS_SHORT = "p:s:u:t:Uv"
+    OPTS_LONG = ["password=", "username=", "token=",
+                 "unattended"]
     opts, args = getopt(sys.argv[1:], OPTS_SHORT, OPTS_LONG)
 
     username = None
     password = None
-    cookies = None
-    cookie_output_filename = None
+    token = None
     unattended = False
-    keep_original = False
 
     for o, a in opts:
-        if o in ("-s", "--session-id"):
-            cookies = {"PHPSESSID": a}
-        elif o in ("-c", "--cookie-jar"):
-            cookie_output_filename = a
+        if o in ("-u", "--token"):
+            token = a
         elif o in ("-u", "--username"):
-            if cookies:
-                print("Using PHPSESSID, ignoring username supplied from the"
-                      "command line")
+            if token:
+                print("Using an access token, ignoring username supplied from"
+                      "the command line")
             else:
                 username = a
         elif o in ("-p", "--password"):
-            if cookies:
-                print("Using PHPSESSID, ignoring password supplied from the"
-                      "command line" % cookies.filename)
+            if token:
+                print("Using an access token, ignoring password supplied from"
+                      "the command line")
             else:
                 password = a
         elif o in ("-U", "--unattended"):
             unattended = True
 
     if len(args) < 1:
-        print("usage: %s [-s session_id] URL..." %
+        print("usage: %s [-t token] ID..." %
               os.path.basename(sys.argv[0]), file=sys.stderr)
     else:
         if username and password:
-            cookies = pixiv_login(username, password)
-        elif not unattended and not cookies:
+            token = pixiv_login(username, password)
+        elif not unattended and not token:
             if not username:
                 username = input("Pixiv ID: ")
             if not password:
                 password = getpass("Password: ")
-            cookies = pixiv_login(username, password)
+            token = pixiv_login(username, password)
 
         for arg in args:
-            dl(arg, cookies)
-
-        if (cookies and cookie_output_filename):
-            save_cookies(cookies, cookie_output_filename)
+            if dl(arg, token):
+                print("{} done".format(arg))
+            else:
+                print("{} failed".format(arg))
 
 if __name__ == "__main__":
     main()
